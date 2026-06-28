ab66e443f30176d38cbb873eba68a1e81336994f
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-06-28 18:24:11 -0300

    clase reporte csv

---
+++ src/main/java/reporte/ReporteCSV.java
@@ -0,0 +1,28 @@
+package reporte;
+
+//necesito importar Item para que funcione
+import java.util.*;
+
+public class ReporteCSV extends Reporte {
+	
+  @Override
+  public void generarReporte(List<Item> items) {
+  	escribir(cabecera());
+      for (Item item : items) {
+      	escribir(escribirFila(item));
+      }
+ }
+
+  @Override
+  public String escribirFila(Item item) {
+  	return item.getNombre() + "," +
+  			item.getCantidadVendida() + "," +
+  			item.getPrecioPromedioCobrado() + "\n";
+  }
+  
+  @Override
+  public String cabecera() {
+  	return "Nombre,Vendidos,PrecioPromedio\n";
+  }
+  
+}
