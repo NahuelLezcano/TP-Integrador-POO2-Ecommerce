e607448f5eeb0c256355e85dd012c1d373e53a3a
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-06-28 22:29:45 -0300

    clase catalogo

--- src/main/java/catalogo/Catalogo.java
+++ src/main/java/catalogo/Catalogo.java
@@ -1,5 +1,30 @@
 package catalogo;
 
-public class Catalogo {
+import java.util.*;
 
+public class Catalogo {
+	
+    private List<Item> items = new ArrayList<>();
+    
+    public Catalogo(List<Item> items) {
+    	this.items = items;
+    }
+
+    public void agregarItem(Item item) {
+        items.add(item);
+    }
+
+    public void removerItem(Item item) {
+        items.remove(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+    
+    public int cantidadDeItems() {
+    	return getItems().size();
+    }
+    
 }
+
