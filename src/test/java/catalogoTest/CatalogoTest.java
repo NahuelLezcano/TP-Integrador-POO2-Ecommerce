656a083bc267eed8b305382df5eeedc0d9fedfa1
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-06-28 23:15:20 -0300

    base para catalogo test

--- src/test/java/catalogoTest/CatalogoTest.java
+++ src/test/java/catalogoTest/CatalogoTest.java
@@ -2,13 +2,29 @@ package catalogoTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import catalogo.*;
 
 class CatalogoTest {
+	
+	Catalogo catalogo;
+	Producto celular;
+	Producto auriculares;
+	Producto memoria;
+	Paquete celularNuevo;
+	Paquete celularExpandido;
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		catalogo = new Catalogo(List.of(celularExpandido, celularNuevo));
+		celular = new Producto("AR528", "Samsung Galaxy A20", "Samsung", "Tecnología", 169, 127000, "Celular duradero");
+		auriculares = new Producto("BR111", "Galaxy Buds 4 Pro", "Samsung", "Accesorio", 10, 620000, "Sonido cálido");
+		memoria = new Producto("NY871", "Msd Kingston Canvas Select Plus", "Kingston", "Memoria", 1, 33500, "Velocidad de lectura de hasta 100 MB/s");
+		celularNuevo = new Paquete("Pack celular nuevo", "Incluye un celular y auriculares", 10, List.of(celular, auriculares));
+		celularExpandido = new Paquete("Pack celular nuevo con más memoria", "Incluye un celular, auriculares y memoria msd", 20, List.of(celularNuevo, memoria));
 	}
 
 	@Test
