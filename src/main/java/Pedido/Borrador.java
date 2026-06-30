2e9e3409e674eae018319e3b4e6af4c238072521
Nahuel Lezcano <lezcanoes2100@gmail.com>
2026-06-30 16:49:57 -0300

    Agrega el borrador

---
+++ src/main/java/Pedido/Borrador.java
@@ -0,0 +1,32 @@
+package Pedido;
+
+import catalogo.Item;
+
+public class Borrador extends Estado {
+
+    public Borrador(Pedido pedido) {
+        super(pedido);
+    }
+
+    @Override
+    public void agregarItem(Item item) {
+        pedido.agregarItemAlPedido(item);
+    }
+
+    @Override
+    public void removerItem(Item item) {
+        pedido.quitarItemAlPedido(item);
+    }
+
+    @Override
+    public String confirmar() {
+        pedido.cambiarEstado(new Pago(pedido));
+        return "El borrador del pedido se a confirmado";
+    }
+
+    @Override
+    public String cancelar() {
+        pedido.cambiarEstado(new Cancelado(pedido));
+        return "El borrador del pedido se a cancelado";
+    }
+}
