5dc0d59fba7865f9164f09c6f4a3c01a05de5342
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-07-08 19:13:33 -0300

    clase observador

--- src/main/java/Pedido/Pedido.java
+++ src/main/java/Pedido/Pedido.java
@@ -2,14 +2,19 @@ package Pedido;
 
 import Tienda.*;
 import catalogo.Item;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import notificacion.*;
 
 public class Pedido {
 
     private Map<Item, Integer> items = new HashMap<>();
     private Tienda tienda;
     private Estado estadoDelPedido = new Borrador(this);
+    private List<Observador> observadores = new ArrayList<>();
 
     public Pedido(Tienda tienda) {
         this.tienda = tienda;
@@ -32,11 +37,13 @@ public class Pedido {
         return estadoDelPedido.cancelar();
     }
 
-    protected void cambiarEstado(Estado estado) {
-        estadoDelPedido = estado;
+    protected void cambiarEstado(Estado nuevoEstado) {
+        Estado anterior = this.estadoDelPedido;
+        this.estadoDelPedido = nuevoEstado;
+        notificarObservadores(anterior, nuevoEstado);
     }
 
-    public Tienda getTienda() {
+	public Tienda getTienda() {
         return tienda;
     }
 
@@ -70,4 +77,19 @@ public class Pedido {
             return stockActual - cantidad;
         });
     }
+
+	public List<Observador> getObservadores() {
+		return observadores;
+	}
+	
+	public void agregarObservador(Observador observador) {
+		observadores.add(observador);
+	}
+	
+	private void notificarObservadores(Estado anterior, Estado nuevo) {
+	    for (Observador o : observadores) {
+	        o.actualizar(this, anterior, nuevo);
+	    }
+	}
+	
 }
