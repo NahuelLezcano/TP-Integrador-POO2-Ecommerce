af106349e7b864a70eae9755e35b55b2c2271538
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-07-11 15:26:59 -0300

    nueva factura

--- src/main/java/factura/Factura.java
+++ src/main/java/factura/Factura.java
@@ -5,17 +5,26 @@ import java.util.*;
 import catalogo.Item;
 
 public class Factura {
-	List<Item> items;
-	
-	public Factura() {
-		this.items = new ArrayList<Item>();
+
+	private Map<Item, Integer> items;
+
+	public Factura(Map<Item, Integer> items) {
+		this.items = new HashMap<>(items);
 	}
 
-	public void agregarItem(Item item) {
-		this.items.add(item);
+	public Map<Item, Integer> getItems() {
+		return items;
 	}
-	
+
 	public double montoTotal() {
-		return this.items.stream().mapToDouble( i -> i.getPrecioFinal()).sum();
+		return items.entrySet().stream().mapToDouble(items -> items.getKey().getPrecioFinal() * items.getValue()).sum();
+	}
+
+	public String desgloseDeFactura() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Factura:\n");
+		items.forEach((item, cantidad) -> sb.append(item.getNombre()).append(" x").append(cantidad).append("\n"));
+		sb.append("Monto Total: $").append(montoTotal());
+		return sb.toString();
 	}
 }
