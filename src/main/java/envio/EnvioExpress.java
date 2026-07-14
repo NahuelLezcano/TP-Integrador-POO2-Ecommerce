26f30976746065c212e31dc9a607371fb41fca58
Nahuel Lezcano <lezcanoes2100@gmail.com>
2026-07-14 17:04:11 -0300

    clase envio express

---
+++ src/main/java/envio/EnvioExpress.java
@@ -0,0 +1,24 @@
+package envio;
+
+import Pedido.Pedido;
+
+public class EnvioExpress implements MetodoDeEnvio {
+
+    private double cargoBase;
+
+    public EnvioExpress(double cargoBase) {
+        this.cargoBase = cargoBase;
+    }
+
+    @Override
+    public double costoDeEnvio(Pedido pedido) {
+        int valorTotalDelPedido = pedido.montoTotal();
+        double porcentaje = 0.10; // Elegí 10%
+        return (valorTotalDelPedido * porcentaje) + cargoBase;
+    }
+
+    @Override
+    public int estimacionDeDias(Pedido pedido) {
+        return 1;
+    }
+}
