11d7197adc92cc64a0602f52ee11898c8fcfe82a
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-07-15 20:03:17 -0300

    Envio Estandar

--- src/main/java/envio/EnvioEstandar.java
+++ src/main/java/envio/EnvioEstandar.java
@@ -4,21 +4,34 @@ import Pedido.Pedido;
 
 public class EnvioEstandar implements MetodoDeEnvio {
 
-    private Direccion direccionEnvio;
-    private CorreoArgentino correoArgentino;
+	private Direccion direccionEnvio;
+	private CorreoArgentino correoArgentino;
 
-    public EnvioEstandar(Direccion direccionEnvio, CorreoArgentino correoArgentino) {
-        this.direccionEnvio = direccionEnvio;
-        this.correoArgentino = correoArgentino;
-    }
+	public EnvioEstandar(Direccion direccionEnvio, CorreoArgentino correoArgentino) {
+		this.direccionEnvio = direccionEnvio;
+		this.correoArgentino = correoArgentino;
+	}
 
-    @Override
-    public double costoDeEnvio(Pedido pedido) {
-        return correoArgentino.estimarEnvio(pedido.pesoTotalPedido(), direccionEnvio) ;
-    }
+	@Override
+	public double costoDeEnvio(Pedido pedido) {
+		Direccion direccionDestino = pedido.getTienda().getDeposito().getDireccion();
 
-    @Override
-    public int estimacionDeDias(Pedido pedido) {
-        return 0;  // Falta completar. La estimación de días es fija entre 5 y 7 días hábiles
-    }
+		return getCorreoArgentino().estimarEnvio(pedido.pesoTotalPedido(), direccionDestino);
+	}
+
+	@Override
+	public int estimacionDeDias(Pedido pedido) {
+		Direccion direccionDeposito = pedido.getTienda().getDeposito().getDireccion();
+		double distanciaKm = direccionEnvio.distanciaEnKmA(direccionDeposito);
+
+		// Cada 15 km suma un día desde 5 hasta 7
+		int dias = 5 + (int) (distanciaKm / 15);
+
+		return Math.min(dias, 7);
+	}
+
+	private CorreoArgentino getCorreoArgentino() {
+		return correoArgentino;
+	}
+
 }
