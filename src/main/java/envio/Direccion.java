4731bfe4e0b5bf6d13a8a14a0f0d044d546fdfc3
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2026-07-15 14:51:00 -0300

    clase direccion

--- src/main/java/envio/Direccion.java
+++ src/main/java/envio/Direccion.java
@@ -1,4 +1,19 @@
 package envio;
 
 public class Direccion {
+
+    int coordX;
+    int coordY;
+
+    public Direccion(int coordX, int coordY) {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+
+    public double distanciaEnKmA(Direccion direccionDestino) {
+        int diferenciaCoordX = this.coordX - direccionDestino.coordX;
+        int diferenciaCoordY = this.coordY - direccionDestino.coordY;
+        return Math.sqrt(diferenciaCoordX * diferenciaCoordX + diferenciaCoordY * diferenciaCoordY);
+    }
+
 }
